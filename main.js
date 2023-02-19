1b06030468883fb17ead524c1ef9f75300d823d9
LUKEWHITTAKER3 <whitta21@lancaster.ac.uk>
2023-02-19 03:07:24 +0000

    Implemented smooth scrolling animations

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-const MAX_START = 100;
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((e) => {
+        console.log(e);
+        if (e.isIntersecting) {
+            e.target.classList.add('show');
+        } else {
+            e.target.classList.remove('show');
+        }
+    });
+})
 
+const scrollerSections = document.querySelectorAll('.hidden');
+scrollerSections.forEach((s) => observer.observe(s));
